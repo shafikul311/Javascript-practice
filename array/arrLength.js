8f8859c84693443f8f11653aec3e39b73e269665
“shafikulislam067@gmail.comgit config --global user.name “shafikul islam <shafikulislam067@gmail.comgit config --global user.email shafikulislam067@gmail.com>
2021-11-26 07:53:00 +0600

    splice

---
+++ array/arrLength.js
@@ -0,0 +1,16 @@
+// const fruits = ['Apple', 'Orange', 'Strawberry'];
+// fruits.length = 6;
+
+// const arrLn = fruits.length
+
+// console.log(arrLn)
+
+// console.log(fruits)
+
+let scores = [1, 2, 3, 4, 5];
+
+// const dlScore = scores.splice(0 , 3)
+// console.log(dlScore)
+
+const dlS = scores.slice(0 , 3)
+console.log(dlS)
