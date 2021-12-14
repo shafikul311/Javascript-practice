5be9f5f29496b8e42d631294a0cfba669ed6dff2
“shafikulislam067@gmail.comgit config --global user.name “shafikul islam <shafikulislam067@gmail.comgit config --global user.email shafikulislam067@gmail.com>
2021-12-14 08:25:07 +0600

    include and trim string inside

---
+++ jsmethod/include.js
@@ -0,0 +1,15 @@
+
+let email = "  shafikul @22gmail.com ";
+
+
+let et = email.replace(/\s/g, "");
+console.log(et);
+
+
+
+console.log(email.includes("@"))
+
+let num = 234234
+console.log(num)
+let nu = num.toString()
+console.log(nu)
---
+++ this/thisFunction.js
@@ -0,0 +1,25 @@
+// const person1 = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     id: 5566,
+//     fullName : function() {
+//       return this.firstName + " " + this.lastName;
+//     }
+//   };
+
+//   console.log(person.fullName())
+
+
+
+
+
+//   const person2 = {
+//        firstName = 'shafikul',
+//        lastName = 'islam',
+//        id: 1210,
+       
+//   }
+
+ 
+
+//   console.log(person2.fullName)
