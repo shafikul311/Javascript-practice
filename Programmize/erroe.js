82474f2bc3f29ffaff5d6728c7532c4bd2b022dd
“shafikulislam067@gmail.comgit config --global user.name “shafikul islam <shafikulislam067@gmail.comgit config --global user.email shafikulislam067@gmail.com>
2022-01-06 16:34:20 +0600

    sort

---
+++ Programmize/erroe.js
@@ -0,0 +1,13 @@
+const numerator= 100, denominator = 'a';
+
+try {
+     console.log(numerator/denominator);
+     console.log(a);
+}
+catch(error) {
+    console.log('An error caught'); 
+    console.log('Error message: ' + error);  
+}
+// finally {
+//      console.log('Finally will execute every time');
+// }
---
+++ Programmize/sort.js
@@ -0,0 +1,25 @@
+const number = [40, 100, 1, 5, 25, 10];
+
+
+// small to bit 
+ const sortNumber = number.sort(function (a, b){
+    return a- b
+})
+
+
+console.log(sortNumber)
+
+// big to small 
+const bigSort = number.sort(function (a, b){
+    return b- a;
+})
+
+
+console.log(bigSort)
+
+
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"]; 
+ const charSort = fruits.sort()
+
+ console.log(charSort)
