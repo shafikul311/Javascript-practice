00ab0647b8898f15742fca328cf33e9a4ad5bb97
“shafikulislam067@gmail.comgit config --global user.name “shafikul islam <shafikulislam067@gmail.comgit config --global user.email shafikulislam067@gmail.com>
2022-02-19 08:58:07 +0600

    find filter method recape

---
+++ methodJs/method.js
@@ -0,0 +1,31 @@
+const users = [
+    {id: 1, name: 'Shafikul', email: 'shfikulislam067@gmail.com', age: 23},
+    {id: 2, name: 'Milos', email: 'miloslam067@gmail.com', age: 26},
+    {id: 3, name: 'Ivan', email: 'ivan@gmail.com', age: 18},
+    {id: 4, name: 'Bora' , email: 'bora@gmail.com' , age: 21},
+    {id: 5, name: 'Marina', email: 'marina@gmail.com', age: 28},
+    {id: 6, name: 'Anika', email: 'anika@gmail.com', age:19},
+    {id: 7, name: 'Arbe', email: 'arbe@gmail.com', age: 4}
+];
+
+
+//find Method
+
+const findUnder18 = users.find(user => user.age< 18);
+// console.log(findUnder18)
+
+//over 18
+const findOver18 = users.find(user => user.age>18);
+// console.log(findOver18)
+
+
+// filter method
+
+const filterUnder20 = users.filter( user => user.age< 20 );
+// console.log(filterUnder20)
+
+// over 20 
+const filterOver20 = users.filter( user => user.age> 20 );
+console.log(filterOver20, filterOver20.length)
+
+
