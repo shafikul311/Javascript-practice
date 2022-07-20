def30f8b5c1c8289c53c01189c170a375944e6b8
“shafikulislam067@gmail.comgit config --global user.name “shafikul islam <shafikulislam067@gmail.comgit config --global user.email shafikulislam067@gmail.com>
2022-07-20 06:57:57 +0600

    remove duplicate from array

---
+++ duplicateRemove/arrayDuplicateRemove.js
@@ -0,0 +1,12 @@
+let arr = [ 1, 3,1, -1,-5, 5, 5, 6, 8, 6, 6, 7, 8, 4, 3, 2, 7];
+
+uniqNum= [];
+for(let i = 0; i<arr.length; i++){
+    let element = arr[i];
+    if(uniqNum.indexOf(element)== -1){
+        uniqNum.push(element)
+    }
+}
+const uniqSort = uniqNum.sort()
+
+console.log('uniqNum is ', uniqSort);
