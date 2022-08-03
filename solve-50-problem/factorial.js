b425fcefad6b837aef888fafa066173edf166340
“shafikulislam067@gmail.comgit config --global user.name “shafikul islam <shafikulislam067@gmail.comgit config --global user.email shafikulislam067@gmail.com>
2022-08-03 08:59:19 +0600

    factorail num using for lo

---
+++ solve-50-problem/factorial.js
@@ -0,0 +1,13 @@
+function factorial(num) {
+    if (num === 0 || num === 1)
+      return 1;
+    for (var i = num - 1; i >= 1; i--) {
+      num *= i;
+    }
+    return num;
+  }
+
+  const fact = factorial(6);
+  
+
+  console.log(fact)
