d35dfc9d7eb08d2f1123497137c43459f3e729c1
“shafikulislam067@gmail.comgit config --global user.name “shafikul islam <shafikulislam067@gmail.comgit config --global user.email shafikulislam067@gmail.com>
2022-10-05 08:40:07 +0600

    array practice

---
+++ array/at.js
@@ -0,0 +1,22 @@
+
+const arr = [2, 3, 4, 3, 7, 8]
+
+// console.log(arr.at(-2))
+const cart = ['apple', 'banana', 'pear', 'orange', 'papaya'];
+// console.log(cart.at(-2))
+// console.log(cart.at(0))
+
+cart.pop()
+// console.log(cart)
+
+cart.push(5)
+
+// cart.push('banana@')
+
+arr.unshift(6)
+
+arr.shift()
+
+cart.fill("Action", 2, 4)
+console.log(cart)
+// console.log(cart)
