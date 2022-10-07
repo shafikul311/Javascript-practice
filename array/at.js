5980fa13631b36f6cd071e442b23441a5429e699
“shafikulislam067@gmail.comgit config --global user.name “shafikul islam <shafikulislam067@gmail.comgit config --global user.email shafikulislam067@gmail.com>
2022-10-07 19:02:34 +0600

    array filter reduce

--- array/at.js
+++ array/at.js
@@ -1,5 +1,5 @@
 
-const arr = [2, 3, 4, 3, 7, 8]
+const arr = [2, 2, 3, 3, 4, 3, 5, 6, 10, 10, 7, 8]
 
 // console.log(arr.at(-2))
 const cart = ['apple', 'banana', 'pear', 'orange', 'papaya'];
@@ -18,5 +18,24 @@ arr.unshift(6)
 arr.shift()
 
 cart.fill("Action", 2, 4)
-console.log(cart)
 // console.log(cart)
+// console.log(cart)
+
+// arr.reverse()
+const incl = arr.includes(3)
+// console.log(incl)
+
+const map = arr.map(num => num * 5)
+
+// console.log(map)
+
+const reduce = arr.reduce((acc, num) => acc + num , 0)
+
+// console.log(reduce)
+const filter = arr.filter(num => num > 5)
+console.log(filter)
+
+const find = arr.find(num => num > 4)
+// console.log(find)
+
+console.log(arr)
